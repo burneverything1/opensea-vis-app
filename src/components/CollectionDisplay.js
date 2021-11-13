b0f387582bf00c1349d3d2186e8363803dd4a195
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-11-13 15:47:03 -0500

    working passing contract data as props

--- src/components/CollectionDisplay.js
+++ src/components/CollectionDisplay.js
@@ -1,20 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material'
 
-const CollectionDisplay = ({ collection_Data }) => {
-    const [collectionData, setCollectionData] = useState(collection_Data)
+const CollectionDisplay = ({ collection_address, col_discord_url }) => {
+    const [colAddress, setColAddress] = useState(collection_address)
+    const [colDiscordUrl, setColDiscordUrl] = useState(col_discord_url)
 
     
+    // update row data after props are passed from api call
+    useEffect(() => {
+        setColAddress(collection_address)
+    }, [collection_address])
+
+    useEffect(() => {
+        setColDiscordUrl(col_discord_url)
+    }, [col_discord_url])
 
     return (
         <Table>
             <TableHead>
                 <TableRow>
-                    <TableCell>Info</TableCell>
-                    <TableCell align='right'>Result</TableCell>
+                    <TableCell>Stat</TableCell>
+                    <TableCell align='right'>Data</TableCell>
                 </TableRow>
             </TableHead>
-            <TableBody></TableBody>
+            <TableBody>
+                <TableRow key='address'>
+                    <TableCell>Address</TableCell>
+                    <TableCell align='right'>{colAddress}</TableCell>
+                </TableRow>
+                <TableRow key='discord'>
+                    <TableCell>Discord</TableCell>
+                    <TableCell align='right'>{colDiscordUrl}</TableCell>
+                </TableRow>
+            </TableBody>
         </Table>
     )
 }
